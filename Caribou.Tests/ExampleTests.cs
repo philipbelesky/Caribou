dd4311e5b16a8203e6fe0e1db348cc955e33317b
Philip Belesky <philip@belesky.com>
2021-04-13 10:59:35 +1000

    Remove files

--- Caribou.Tests/ExampleTests.cs
+++ Caribou.Tests/ExampleTests.cs
@@ -4,6 +4,7 @@ namespace Caribou.Tests
     using System;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using Rhino.Geometry;
+    using Caribou.Processing;
 
     [TestClass]
     public class ExampleTests
@@ -12,39 +13,17 @@ namespace Caribou.Tests
         /// Transform a brep using a translation
         /// </summary>
         [TestMethod]
-        public void TestBrepTranslation()
+        public void TestParseA()
         {
-            // Arrange
-            var bb = new BoundingBox(new Point3d(0, 0, 0), new Point3d(100, 100, 100));
-            var brep = bb.ToBrep();
-            var t = Transform.Translation(new Vector3d(30, 40, 50));
-
-            // Act
-            brep.Transform(t);
-
-            // Assert
-            Assert.AreEqual(brep.GetBoundingBox(true).Center, new Point3d(80, 90, 100));
+            var result = XMLParsing.ParserA();
+            Assert.AreEqual(result, "OKA");
         }
 
-        /// <summary>
-        /// Intersect sphere with a plane to generate a circle
-        /// </summary>
         [TestMethod]
-        public void TestBrepIntersection()
+        public void TestParseB()
         {
-            // Arrange
-            var radius = 4.0;
-            using (var brep = Brep.CreateFromSphere(new Sphere(new Point3d(), radius)))
-            {
-                var cuttingPlane = Plane.WorldXY;
-
-                // Act
-                Rhino.Geometry.Intersect.Intersection.BrepPlane(brep, cuttingPlane, 0.001, out var curves, out var points);
-
-                // Assert
-                Assert.AreEqual(1, curves.Length, "Wrong curve count");
-                Assert.AreEqual(2 * Math.PI * radius, curves[0].GetLength(), "Wrong curve length");
-            }
+            var result = XMLParsing.ParserB();
+            Assert.AreEqual(result, "OKB");
         }
     }
 }
