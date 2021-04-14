4d18a23715e81e320dc83b4cebd2696a970a756a
Philip Belesky <philip@belesky.com>
2021-04-14 21:29:14 +1000

    Add an actual parsing method

--- Caribou.Tests/MelbourneTests.cs
+++ Caribou.Tests/MelbourneTests.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Caribou.Tests
+﻿namespace Caribou.Tests
 {
-    class MelbourneTests
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MelbourneTests
     {
         private string melbourneFile = Properties.Resources.MelbourneOSM;
+        private Dictionary<string, string> restarauntsAndJewelers = new Dictionary<string, string>
+        {
+            { "amenity", "restaurant" },
+            { "craft", "jeweller" },
+        };
+
+        [TestMethod]
+        public void ParseMelbourneA()
+        {
+            var result = Caribou.Processing.FindNodes.FindByFeaturesA(restarauntsAndJewelers, melbourneFile);
+            Assert.AreEqual(result["amenity:restaurant"].Count, 173);
+            Assert.AreEqual(result["amenity:restaurant"][0].Latitude, -37.8134515);
+            Assert.AreEqual(result["craft:jeweller"].Count, 1);
+            Assert.AreEqual(result["craft:jeweller"][0].Longitude, 144.9658410);
+        }
     }
 }
---
+++ Caribou/Processing/FindNodesByFeature.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Caribou.Processing
+{
+    public struct Coords
+    {
+        public Coords(double x, double y)
+        {
+            Latitude = x;
+            Longitude = y;
+        }
+
+        public double Latitude { get; }
+        public double Longitude { get; }
+
+        public override string ToString() => $"({Latitude}, {Longitude})";
+    }       
+
+    public class FindNodes
+    {
+        public static Dictionary<string, List<Coords>> FindByFeaturesA(Dictionary<string, string> featuresSpecified, string xmlContents)
+        {
+            // Output
+            var matches = new Dictionary<string, List<Coords>>();
+            foreach (string key in featuresSpecified.Keys)
+            {
+                matches[key + ":" + featuresSpecified[key]] = new List<Coords>();
+            }
+
+            using (XmlReader reader = XmlReader.Create(new StringReader(xmlContents)))
+            {
+                // XmlReader is forward-only so need to track the lat/long of the parent element to use if we find a match
+                double latitude = 0.0;
+                double longitude = 0.0;
+                string tagKey;
+                string tagValue;
+                string[] featureKeys = featuresSpecified.Keys.ToArray(); // TODO: is this actually faster than just checking the dict?
+                
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        if (reader.Name == "node")
+                        {
+                            latitude = Convert.ToDouble(reader.GetAttribute("lat"));
+                            longitude = Convert.ToDouble(reader.GetAttribute("lon"));
+                        }
+                        else if (reader.Name == "tag")
+                        {
+                            tagKey = reader.GetAttribute("k");
+                            if (featureKeys.Contains(tagKey))
+                            {
+                                tagValue = reader.GetAttribute("v");
+                                if (tagValue == featuresSpecified[tagKey])
+                                {
+                                    matches[tagKey + ":" + tagValue].Add(new Coords(latitude, longitude));
+                                }
+                                // TODO: skip read to next <node> value rather than continue reading subsequent sibling tags
+                            }
+                        }
+                    }
+                }
+            }
+            return matches;
+        }
+    }
+}
