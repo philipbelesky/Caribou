132977955c373e025ecea13fc6bd0c58baee1be2
Philip Belesky <philip@belesky.com>
2021-04-25 13:24:55 +1000

    Setup basic components for feature extraction

---
+++ Caribou/Components/SpecifyFeaturesComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace Caribou.Components
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Caribou.Properties;
+    using Grasshopper.Kernel;
+
+    public class SpecifyFeaturesComponent : CaribouComponent
+    {
+        public SpecifyFeaturesComponent() : base ("Specify features and subfeatures to extract", "Set Features", "TODO", "OSM") { }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            // None needed
+        }
+
+        protected override void CaribouRegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("OSM Features", "F", "A list of OSM features and subfeatures", GH_ParamAccess.list);
+        }
+
+        protected override void CaribouSolveInstance(IGH_DataAccess da)
+        {
+
+        }
+
+        public override Guid ComponentGuid => new Guid("cc8d82ba-f381-46ee-8014-7e2d1bff824c");
+
+        protected override System.Drawing.Bitmap Icon => Resources.icons_icon_test;
+    }
+}
---
+++ Caribou/Components/SpecifyTagValuesComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace Caribou.Components
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Caribou.Properties;
+    using Grasshopper.Kernel;
+
+    public class SpecifyTagValuesComponent : CaribouComponent
+    {
+        public SpecifyTagValuesComponent() : base ("Specify tags and values to extract", "Set Tags", "TODO", "OSM") { }
+
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            // None needed
+        }
+
+        protected override void CaribouRegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("OSM Features", "F", "A list of OSM tags and keys", GH_ParamAccess.list);
+        }
+
+        protected override void CaribouSolveInstance(IGH_DataAccess da)
+        {
+
+        }
+
+        public override Guid ComponentGuid => new Guid("19eb5d29-d1d1-43bf-aa40-8d9694c03481");
+
+        protected override System.Drawing.Bitmap Icon => Resources.icons_icon_test;
+    }
+}
