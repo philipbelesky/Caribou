4ec64554bd9e02eaa462391ec7955d2051a9c40d
Philip Belesky <philip@belesky.com>
2021-05-21 19:48:07 +1000

    Initial "hello world" Eto setup

--- Caribou/Components/SpecifyFeaturesComponent.cs
+++ Caribou/Components/SpecifyFeaturesComponent.cs
@@ -1,34 +1,90 @@
 ﻿namespace Caribou.Components
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Windows.Forms;
     using Caribou.Properties;
+    using GH_IO.Serialization;
+    using Grasshopper.GUI;
+    using Grasshopper.GUI.Canvas;
     using Grasshopper.Kernel;
+    using Grasshopper.Kernel.Attributes;
 
     /// <summary>Provides a GUI interface to selecting/specifying predefined OSM features/subfeatures.</summary>
-    //public class SpecifyFeaturesComponent : CaribouComponent
-    //{
-    //    public SpecifyFeaturesComponent() : base("Specify features and subfeatures to extract", "Set Features", "TODO", "OSM") { }
+    public class SpecifyFeaturesComponent : CaribouComponent
+    {
+        private string textOfKeyValueSelected = "TODO key:value";
+        private SpecifyFeaturesForm pickerForm;
 
-    //    protected override void RegisterInputParams(GH_InputParamManager pManager)
-    //    {
-    //        // None needed
-    //    }
+        public SpecifyFeaturesComponent() : base("Specify Features/SubFeatures", "Specify Features",
+            "Provides a graphical interface (via double-click or right-click menu) to specify a list of OSM features.", "OSM") 
+        {
+        }
 
-    //    protected override void CaribouRegisterOutputParams(GH_OutputParamManager pManager)
-    //    {
-    //        pManager.AddTextParameter("OSM Features", "F", "A list of OSM features and subfeatures", GH_ParamAccess.list);
-    //    }
+        protected override void RegisterInputParams(GH_InputParamManager pManager) { }
 
-    //    protected override void CaribouSolveInstance(IGH_DataAccess da)
-    //    {
-    //    }
+        protected override void CaribouRegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("OSM Features", "F", "A list of OSM features and subfeatures", GH_ParamAccess.list);
+        }
 
-    //    public override Guid ComponentGuid => new Guid("cc8d82ba-f381-46ee-8014-7e2d1bff824c");
+        protected override void CaribouSolveInstance(IGH_DataAccess da)
+        {
+        }
 
-    //    protected override System.Drawing.Bitmap Icon => Resources.icons_select;
-    //}
+        public override void CreateAttributes()
+        {
+            m_attributes = new MySpecialComponentAttributes(this); // Add custom attrributes so double-clicks open form
+        }
+
+        private class MySpecialComponentAttributes : GH_ComponentAttributes
+        {
+            public MySpecialComponentAttributes(IGH_Component component)
+              : base(component) { }
+
+            public override GH_ObjectResponse RespondToMouseDoubleClick(GH_Canvas sender, GH_CanvasMouseEvent e)
+            {
+                (Owner as SpecifyFeaturesComponent)?.OpenFeaturePicker();
+                return GH_ObjectResponse.Handled;
+            }
+        }
+
+        protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
+        {
+            Menu_AppendItem(menu, "Select Features", this.OpenFeaturePickerFromMenu, null, true, false);
+            Menu_AppendSeparator(menu);
+        }
+
+        private void OpenFeaturePickerFromMenu(object sender, EventArgs e)
+        {
+            OpenFeaturePicker();
+        }
+
+        private void OpenFeaturePicker()
+        {
+            this.pickerForm = new SpecifyFeaturesForm();
+
+            //var mousePos = Mouse.Position;
+            //int x = (int)mousePos.X + 20;
+            //int y = (int)mousePos.Y - 160;
+            //this.pickerForm.Location = new Eto.Drawing.Point(x, y);
+
+            this.pickerForm.Show();
+        }
+
+        public override bool Read(GH_IReader reader) // Add message below component
+        {
+            this.Message = textOfKeyValueSelected;
+            return base.Read(reader);
+        }
+
+        public override void AddedToDocument(GH_Document document) // Add message below component
+        {
+            this.Message = textOfKeyValueSelected;
+            base.AddedToDocument(document);
+        }
+
+        public override Guid ComponentGuid => new Guid("cc8d82ba-f381-46ee-8014-7e2d1bff824c");
+
+        protected override System.Drawing.Bitmap Icon => Resources.icons_select;
+    }
 }
---
+++ Caribou/Components/SpecifyFeaturesForm.cs
@@ -0,0 +1,20 @@
+﻿namespace Caribou.Components
+{
+    using System;
+    using Rhino.UI;
+    using Eto.Drawing;
+    using Eto.Forms;
+
+    public class SpecifyFeaturesForm : Form
+    {
+        public SpecifyFeaturesForm()
+        {
+            // sets the client (inner) size of the window for your content
+            this.ClientSize = new Eto.Drawing.Size(600, 800);
+            this.Padding = 10;
+
+            this.Title = "Select Features and Sub-Features";
+            this.Resizable = false;
+        }
+    }
+}
