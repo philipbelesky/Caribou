113eae6f08d4c6d69507fc9dc02b457cfe5138b5
Philip Belesky <philip@belesky.com>
2021-05-22 16:47:45 +1000

    Setup HelloWorld layout for feature/subfeature selection

--- Caribou/Components/SpecifyFeaturesForm.cs
+++ Caribou/Components/SpecifyFeaturesForm.cs
@@ -4,13 +4,16 @@
     using Rhino.UI;
     using Eto.Drawing;
     using Eto.Forms;
+    using System.Collections.Generic;
 
     public class SpecifyFeaturesForm : Form
     {
+        private int windowWidth = 600;
+
         public SpecifyFeaturesForm()
         {
             // sets the client (inner) size of the window for your content
-            this.ClientSize = new Eto.Drawing.Size(600, 800);
+            this.ClientSize = new Eto.Drawing.Size(windowWidth, 800);
             this.Padding = 10;
 
             this.Title = "Select Features and Sub-Features";
@@ -21,40 +24,123 @@
                 Padding = 10,
                 Items = {
                     "Hello World!",
+                    ContentSelection(),
+                    ButtomRow(),
                 }
             };
-
-            // create a few commands that can be used for the menu and toolbar
-            var clickMe = new Command { MenuText = "Click Me!", ToolBarText = "Click Me!" };
-            clickMe.Executed += (sender, e) => MessageBox.Show(this, "I was clicked!");
-
-            var quitCommand = new Command { MenuText = "Quit", Shortcut = Application.Instance.CommonModifier | Keys.Q };
-            quitCommand.Executed += (sender, e) => Application.Instance.Quit();
-
-            var aboutCommand = new Command { MenuText = "About..." };
-            aboutCommand.Executed += (sender, e) => new AboutDialog().ShowDialog(this);
-
-            // create menu
-            Menu = new MenuBar
-            {
-                Items =
-                {
-					// File submenu
-					new SubMenuItem { Text = "&File", Items = { clickMe } },
-					// new SubMenuItem { Text = "&Edit", Items = { /* commands/items */ } },
-					// new SubMenuItem { Text = "&View", Items = { /* commands/items */ } },
-				},
-                ApplicationItems =
-                {
-					// application (OS X) or file menu (others)
-					new ButtonMenuItem { Text = "&Preferences..." },
-                },
-                QuitItem = quitCommand,
-                AboutItem = aboutCommand
-            };
-
-            // create toolbar			
-            ToolBar = new ToolBar { Items = { clickMe } };
         }
+
+        private TreeGridView ContentSelection()
+        {
+            var featureSelect = new TreeGridView()
+            {
+                
+            };
+            var column1 = new GridColumn()
+            {
+                HeaderText = "Feature",
+                DataCell = new TextBoxCell(0),
+                Width = 200,
+            };
+            featureSelect.Columns.Add(column1);
+
+            var column2 = new GridColumn()
+            {
+                HeaderText = "SubFeature",
+                DataCell = new TextBoxCell(1),
+                Width = 200,
+            };
+            featureSelect.Columns.Add(column2);
+
+            var column3 = new GridColumn()
+            {
+                HeaderText = "Include",
+                DataCell = new CheckBoxCell(2),
+                Width = 100,
+            };
+            featureSelect.Columns.Add(column3);
+
+            var treeCollection = new TreeGridItemCollection();
+
+            // add one tree grid item with child
+            var parent = new TreeGridItem { Values = new string[] {
+                    "Parent", "Parent prop 1"
+                },
+                Expanded = true
+            };
+            var child = new TreeGridItem { Values = new string[] {
+                    "Child", "Child prop 1"
+                }
+            };
+            parent.Children.Add(child);
+
+            featureSelect.DataStore = new TreeGridItemCollection { parent };
+
+            return featureSelect;
+        }
+
+        private StackLayout ButtomRow()
+        {
+            var updateButton = new Button()
+            {
+                Text = "Update Selection",
+                Width = (windowWidth / 2) - 35,
+                Height = 40,
+
+            };
+            updateButton.Click += (sender, e) => { UpdateAndClose(); };
+
+            var cancelButton = new Button()
+            {
+                Text = "Cancel Update",
+                Width = (windowWidth / 2) - 35,
+                Height = 40,
+            };
+            cancelButton.Click += (sender, e) => { CancelAndClose(); };
+
+            var layout = new StackLayout
+            {
+                Orientation = Orientation.Horizontal,
+                Items = {
+                    updateButton, cancelButton
+                },
+                Spacing = 25,
+                Padding = 0,
+            };
+            return layout;
+        }
+
+        private void UpdateAndClose()
+        {
+            Application.Instance.Quit();
+        }
+
+        private void CancelAndClose()
+        {
+            Application.Instance.Quit();
+        }
+
+        private Dictionary<string, List<string>> testData = new Dictionary<string, List<string>>
+        {
+            { "aerialway", new List<string>() { "A", "B", "C", "D" } },
+            { "aeroway", new List<string>() { "A", "B", "C", "D" } },
+            { "amenity", new List<string>() { "A", "B", "C", "D" } },
+            { "barrier", new List<string>() { "A", "B", "C", "D" } },
+            { "boundary", new List<string>() { "A", "B", "C", "D" } },
+            { "building", new List<string>() { "A", "B", "C", "D" } },
+            { "craft", new List<string>() { "A", "B", "C", "D" } },
+            { "emergency", new List<string>() { "A", "B", "C", "D" } },
+            { "geological", new List<string>() { "A", "B", "C", "D" } },
+            { "healthcare", new List<string>() { "A", "B", "C", "D" } },
+            { "highway", new List<string>() { "A", "B", "C", "D" } },
+            { "historic", new List<string>() { "A", "B", "C", "D" } },
+            { "landuse", new List<string>() { "A", "B", "C", "D" } },
+            { "leisure", new List<string>() { "A", "B", "C", "D" } },
+            { "man_made", new List<string>() { "A", "B", "C", "D" } },
+            { "military", new List<string>() { "A", "B", "C", "D" } },
+            { "natural", new List<string>() { "A", "B", "C", "D" } },
+            { "office", new List<string>() { "A", "B", "C", "D" } },
+            { "place", new List<string>() { "A", "B", "C", "D" } }
+        };
     }
 }
--- Eto.Prototyping/Program.cs
+++ Eto.Prototyping/Program.cs
@@ -15,3 +15,4 @@ namespace Eto.Prototyping
         }
     }
 }
+    
