ead0031f30cfa2b8f65cb2c5baa6f03953321394
Philip Belesky <philip@belesky.com>
2021-05-23 12:13:40 +1000

    Move over data files; split out defined subfeature types

---
+++ Caribou/Models/OSMSelectableFeature.cs
@@ -0,0 +1,27 @@
+﻿namespace Caribou.Models
+{
+    using System;
+    using Caribou.Data;
+
+    /// <summary>
+    /// A OSMMetaData item that will be presented to the user for interaction. It thus has state information and more descriptive information.
+    /// </summary>
+    public class OSMSelectableFeature : OSMMetaData
+    {
+        public bool IsSelected { get; set; }
+        public int NodeCount { get; set; }
+        public int RelationCount { get; set; }
+        public int WayCount { get; set; }
+
+        public OSMSelectableFeature(string id, string name, string explanation,
+                                    int nodeCount, int relationCount, int wayCount,
+                                    OSMSelectableFeature key)
+            : base(id, name, explanation, key)
+        {
+            this.IsSelected = false;
+            this.NodeCount = nodeCount;
+            this.RelationCount = relationCount;
+            this.WayCount = wayCount;
+        }
+    }
+}
