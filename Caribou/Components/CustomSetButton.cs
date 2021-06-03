34ee5a9ac00eab9d8c39a86926fce2b7cbbb97a1
Philip Belesky <philip@belesky.com>
2021-06-03 20:03:32 +1000

    Setup basic structure for the custom button

---
+++ Caribou/Components/CustomSetButton.cs
@@ -0,0 +1,69 @@
+﻿namespace Caribou.Components
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Eto.Forms;
+    using Grasshopper.GUI;
+    using Grasshopper.GUI.Canvas;
+    using Grasshopper.Kernel;
+
+    public class CustomSetButton : Grasshopper.Kernel.Attributes.GH_ComponentAttributes
+    {
+        // Adapted from https://www.grasshopper3d.com/forum/topics/create-radio-button-on-grasshopper-component?commentId=2985220%3AComment%3A835552
+
+        private Action buttonClickHandler; 
+        
+        public CustomSetButton(SpecifyFeaturesComponent owner, Action openFormCallback) : base(owner) {
+            this.buttonClickHandler = openFormCallback;
+        }
+
+        protected override void Layout()
+        {
+            base.Layout();
+
+            System.Drawing.Rectangle rec0 = GH_Convert.ToRectangle(Bounds);
+            rec0.Height += 22;
+            if (rec0.Width < 100)
+            {
+                rec0.Width = 100; // Ensure button has enough space; even if not drawing full names etc
+            }
+
+            System.Drawing.Rectangle rec1 = rec0;
+            rec1.Y = rec1.Bottom - 22;
+            rec1.Height = 22;
+            rec1.Inflate(-2, -2);
+
+            Bounds = rec0;
+            ButtonBounds = rec1;
+        }
+        private System.Drawing.Rectangle ButtonBounds { get; set; }
+
+        protected override void Render(GH_Canvas canvas, System.Drawing.Graphics graphics, GH_CanvasChannel channel)
+        {
+            base.Render(canvas, graphics, channel);
+
+            if (channel == GH_CanvasChannel.Objects)
+            {
+                GH_Capsule button = GH_Capsule.CreateTextCapsule(ButtonBounds, ButtonBounds, GH_Palette.Black, "Specify Features", 2, 0);
+                button.Render(graphics, Selected, Owner.Locked, false);
+                button.Dispose();
+            }
+        }
+        public override GH_ObjectResponse RespondToMouseDown(GH_Canvas sender, GH_CanvasMouseEvent e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                System.Drawing.RectangleF rec = ButtonBounds;
+                if (rec.Contains(e.CanvasLocation))
+                {
+                    this.buttonClickHandler();
+                    return GH_ObjectResponse.Handled;
+                }
+            }
+            return base.RespondToMouseDown(sender, e);
+        }
+    }
+}
